ad5e0a998db3ccf4cba1863d6f0ef4b655f4e5e0
mb4952144@gmail.com <mb4952144@gmail.com>
2023-02-19 12:55:23 +0200

    lab1 testing

--- Unit Testing/Day1/index.js
+++ Unit Testing/Day1/index.js
@@ -1,16 +1,14 @@
-function sum(x,y){
-    return x+y;
-}
-module.exports = {sum};
-
+ 
 
 
 const capitalizeText = (input) => {
-    if (typeof input !== "string"){
-      throw new TypeError("parameter should be string")
-    };
-    return input.toUpperCase();
+  if (typeof input !== "string"){
+    throw new TypeError("parameter should be string")
   };
+  return input[0].toUpperCase() + input.slice(1).toLowerCase() ;
+};
+
+
 module.exports = {capitalizeText};
   
   
--- Unit Testing/Day1/test/test.test.js
+++ Unit Testing/Day1/test/test.test.js
@@ -1,22 +1,42 @@
 const index = require("../index"); // sum, convertToarray
-
 const assertMocha = require("assert"); 
 
-// const expect = require("chai").expect;
+const expect = require("chai").expect;
 // const assert = require("chai").assert;
 // const should = require("chai").should();
 
 
 
-describe("test for sum function using mocha" , function(){
+describe("test first problem lab1" , function(){
     // testcase 
-    it("test that sum function takes two positive numbers and return it's summtion", function(){
-            assertMocha.equal(index.sum(3,3), 6);
+    it.skip("test that the function takes a string and return it after capitalize it ", function(){
+            expect(index.capitalizeText('iti')).to.be.a('string').to.equal('Iti');
+            // not   AssertionError: expected 'HI' to equal 'Hi'+ expected - actual  -HI because  return input.toUpperCase(); 
     })
 
-//     it("test that sum function takes two negative numbers and return it's summtion", function(){
-//         assertMocha.equal(index.sum(-3,-3), -6);
-// })
+ 
+it.skip("test if the function takes number it will throw type error says parameter should be string", () => {
+    
+    expect(()=>index.capitalizeText(123)).Throw('parameter should be string');
+  });
+ 
+it("test if the input iti , the returned value  will not equal to hello", () => {
+    
+    expect(index.capitalizeText('iti')).to.not.equal('hello'); 
+});
+
+
+
+it("test that the function takes a string and return it after capitalize it ", function(){
+    expect(index.capitalizeText('iti')).to.equal('Iti').to.be.a('string');
+    // not   AssertionError: expected 'HI' to equal 'Hi'+ expected - actual  -HI because  return input.toUpperCase(); 
+})
+
+
+
+
 
 
 })
+
+
